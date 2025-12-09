c56c7e88d2fc46357b3c13bcaf6de860b16a3236
StephenCotterrell <stephen@cotterrell.dev>
2025-12-09 09:51:17 -0800

    implemented basic asteroid functionality
    added asteroid.py, asteroidfield.py and added relavant constants to
    constants.py. Implemented in main.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 
 def main():
@@ -11,11 +13,15 @@ def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(
         SCREEN_WIDTH / 2,
         SCREEN_HEIGHT / 2,
     )
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
